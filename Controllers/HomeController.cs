0e8a7617eeaef8f792ec9dd935a84dc0069b95c4
Alan Peake <arpeake83@gmail.com>
2016-08-18 04:25:41 -0400

    Created initial CustomerDB via Migrations.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Themes;
+
+namespace Dcmg.CustomerDB.Controllers
+{
+    public class HomeController:Controller
+    {
+        [Themed]
+        public ActionResult Index()
+        {
+            ViewBag.dt = DateTime.Now;
+            return View();
+        }
+    }
+}
---
+++ Migrations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Data.Migration;
+
+namespace Dcmg.CustomerDB
+{
+    public class Migrations:DataMigrationImpl
+    {
+        public int Create()
+        {
+            //Creating initial Customer DB
+            SchemaBuilder.CreateTable("Customers", table => table
+             .Column("id",System.Data.DbType.Int32,col=>col.PrimaryKey().Identity())
+             .Column("Name",System.Data.DbType.String)
+             .Column("Address",System.Data.DbType.String)
+             .Column("PhoneNumber",System.Data.DbType.String)
+             .Column("PrimaryPOC", System.Data.DbType.String)
+             .Column("SecondaryPOC", System.Data.DbType.String)
+             .Column("PrimaryPOCEmail", System.Data.DbType.String)
+             .Column("SecondaryPOCEmail", System.Data.DbType.String)
+             .Column("EmailAlias", System.Data.DbType.String)
+             .Column("Notes", System.Data.DbType.String)
+            );
+
+            return 1;
+        }
+    }
+}
